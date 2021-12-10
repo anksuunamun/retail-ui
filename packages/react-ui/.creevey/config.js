9d886ab8f8d13f7ce50808853c58f7089cbc8a2c
HelenaIsh <elena-ishmatova@mail.ru>
2021-12-10 11:57:53 +0500

    Revert "test: delete old screenshots"
    This reverts commit fc2bcdd25f53b1dc51aae687480e32f4987db136.

--- packages/react-ui/.creevey/config.js
+++ packages/react-ui/.creevey/config.js
@@ -28,6 +28,46 @@ const config = {
   }),
   diffOptions: { threshold: 0, includeAA: false },
   browsers: {
+    // chrome: {
+    //   browserName: 'chrome',
+    //   viewport: { width: 1024, height: 720 },
+    //   platformName: 'linux',
+    //   _storybookGlobals: {
+    //     theme: 'DEFAULT_THEME_OLD',
+    //   },
+    //   name: 'infrafront/chrome',
+    //   ...capabilities,
+    // },
+    chrome8px: {
+      browserName: 'chrome',
+      viewport: { width: 1024, height: 720 },
+      platformName: 'linux',
+      _storybookGlobals: {
+        theme: 'DEFAULT_THEME',
+      },
+      name: 'infrafront/chrome8px',
+      ...capabilities,
+    },
+    // chromeFlat: {
+    //   browserName: 'chrome',
+    //   viewport: { width: 1024, height: 720 },
+    //   platformName: 'linux',
+    //   _storybookGlobals: {
+    //     theme: 'FLAT_THEME_OLD',
+    //   },
+    //   name: 'infrafront/chromeFlat',
+    //   ...capabilities,
+    // },
+    chromeFlat8px: {
+      browserName: 'chrome',
+      viewport: { width: 1024, height: 720 },
+      platformName: 'linux',
+      _storybookGlobals: {
+        theme: 'FLAT_THEME',
+      },
+      name: 'infrafront/chromeFlat8px',
+      ...capabilities,
+    },
     chromeNewTheme: {
       browserName: 'chrome',
       viewport: { width: 1024, height: 720 },
@@ -36,6 +76,42 @@ const config = {
         theme: 'NEW_THEME',
       },
       name: 'infrafront/chromeNewTheme',
+      ...capabilities,
+    },
+    // firefox: {
+    //   browserName: 'firefox',
+    //   viewport: { width: 1024, height: 720 },
+    //   _storybookGlobals: {
+    //     theme: 'DEFAULT_THEME_OLD',
+    //   },
+    //   name: 'infrafront/firefox',
+    //   ...capabilities,
+    // },
+    firefox8px: {
+      browserName: 'firefox',
+      viewport: { width: 1024, height: 720 },
+      _storybookGlobals: {
+        theme: 'DEFAULT_THEME',
+      },
+      name: 'infrafront/firefox8px',
+      ...capabilities,
+    },
+    // firefoxFlat: {
+    //   browserName: 'firefox',
+    //   viewport: { width: 1024, height: 720 },
+    //   _storybookGlobals: {
+    //     theme: 'FLAT_THEME_OLD',
+    //   },
+    //   name: 'infrafront/firefoxFlat',
+    //   ...capabilities,
+    // },
+    firefoxFlat8px: {
+      browserName: 'firefox',
+      viewport: { width: 1024, height: 720 },
+      _storybookGlobals: {
+        theme: 'FLAT_THEME',
+      },
+      name: 'infrafront/firefoxFlat8px',
       ...capabilities,
     },
     firefoxNewTheme: {
@@ -47,6 +123,15 @@ const config = {
       name: 'infrafront/firefoxNewTheme',
       ...capabilities,
     },
+    // ie11: {
+    //   browserName: 'internet explorer',
+    //   viewport: { width: 1024, height: 720 },
+    //   _storybookGlobals: {
+    //     theme: 'DEFAULT_THEME_OLD',
+    //   },
+    //   name: 'infrafront/ie11',
+    //   ...capabilities,
+
     // NOTE Enable after switch new separate pool for IE to allow test hover
     // 'se:ieOptions': {
     //   enablePersistentHover: true,
@@ -57,6 +142,33 @@ const config = {
     //   'ie.ensureCleanSession': true,
     // },
     // },
+    ie118px: {
+      browserName: 'internet explorer',
+      viewport: { width: 1024, height: 720 },
+      _storybookGlobals: {
+        theme: 'DEFAULT_THEME',
+      },
+      name: 'infrafront/ie118px',
+      ...capabilities,
+    },
+    // ie11Flat: {
+    //   browserName: 'internet explorer',
+    //   viewport: { width: 1024, height: 720 },
+    //   _storybookGlobals: {
+    //     theme: 'FLAT_THEME_OLD',
+    //   },
+    //   name: 'infrafront/ie11Flat',
+    //   ...capabilities,
+    // },
+    ie11Flat8px: {
+      browserName: 'internet explorer',
+      viewport: { width: 1024, height: 720 },
+      _storybookGlobals: {
+        theme: 'FLAT_THEME',
+      },
+      name: 'infrafront/ie11Flat8px',
+      ...capabilities,
+    },
     ie11NewTheme: {
       browserName: 'internet explorer',
       viewport: { width: 1024, height: 720 },
